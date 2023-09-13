011bb676069b35b815e738cb3d08585667638910
Hiroki <s1f102102548@iniad.org>
2023-09-14 04:20:56 +0900

    LEDの信号を送り続けなければいけない問題を考え中

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { error } from "console";
+import * as readline from "readline"
 import { SerialPort } from "serialport"
 
 let path: string = 'COM3';
@@ -12,46 +13,7 @@ export function LED(pinNumber: number){
 
     const identifierCode = 0x90;
     let isOn = false;
-
-    function blink(interval: number = 1000):void{
-
-        setTimeout(()=>{
-            setInterval(()=>{
-                isOn = !isOn;
-    
-                const bufferValue = isOn ? (1 << (pinNumber & 0x07)) : 0x00;
-                const buffer = Buffer.from([identifierCode + (pinNumber >> 3), bufferValue, 0x00]);
-                port.write(buffer, (err)=>{
-                    if(err){
-                        return console.error('Error writing to port: ', err.message);
-                    }
-                    console.log(`LED ${isOn ? 'on' : 'off'} at pin ${pinNumber}`);
-                });
-            }, interval)
-        },2500)
-        
-    }
-
-    function on() {
-
-        const buffer = Buffer.from([identifierCode + (pinNumber >> 3), (1 << (pinNumber & 0x07)), 0x00])
-        port.write(buffer, (err)=>{
-            if(err){
-                return console.error('Error writing to port: ', err.message);
-            }
-            console.log(`LED on at pin ${pinNumber}`)
-        })
-    }
-
-    function off(){
-        const buffer = Buffer.from([identifierCode + (pinNumber >> 3), 0x00, 0x00])
-        port.write(buffer, (err)=>{
-            if(err){
-                return console.error('Error writing to port: ', err.message);
-            }
-            console.log(`LED on at pin ${pinNumber}`)
-        })
-    }
+    let blinkingInterval: NodeJS.Timeout | null = null
 
     port.on('open', ()=>{
         console.log('Arduino connected');
@@ -61,6 +23,53 @@ export function LED(pinNumber: number){
         console.error('Error', err);
     });
 
+    function keepConsoleOpen(){
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+
+        console.log('Press ENTER to exit...');
+
+        rl.on('line', ()=>{
+            if(blinkingInterval){
+                clearInterval(blinkingInterval);
+            }
+            off();
+            rl.close();
+            process.exit(0);
+        })
+    }
+
+    function on() {
+        isOn = true;
+        const bufferValue = 1 << (pinNumber & 0x07);
+        const buffer = Buffer.from([identifierCode + (pinNumber >> 3), bufferValue, 0x00])
+        port.write(buffer, (err)=>{
+            if(err){
+                return console.error('Error writing to port: ', err.message);
+            }
+            console.log(`LED on at pin ${pinNumber}`)
+        })
+        keepConsoleOpen()
+    }
+
+    function off(){
+        isOn = false;
+        const buffer = Buffer.from([identifierCode + (pinNumber >> 3), 0x00, 0x00])
+        port.write(buffer, (err)=>{
+            if(err){
+                return console.error('Error writing to port: ', err.message);
+            }
+            console.log(`LED off at pin ${pinNumber}`)
+        })
+    }
+
+    function blink(interval: number = 1000){
+        blinkingInterval = setInterval(()=>{
+            isOn ? off() : on();
+        },interval);
+    }
 
     return {
         blink,
--- test.ts
+++ test.ts
@@ -2,4 +2,14 @@ import {LED, setup} from './src/index'
 
 setup('COM4');
 const led = LED(13);
-led.blink()
+led.blink(3000)
+
+// led.on();
+// import {setup, LED} from './src/sample'
+
+// setup('COM4')
+// const myLED = LED(13);
+
+// // myLED.blink()
+
+// myLED.on();
