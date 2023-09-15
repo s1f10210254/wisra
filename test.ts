2e87c16c8057b39dc34a37e5f1439e20a217cf65
Hiroki <s1f102102548@iniad.org>
2023-09-15 23:57:56 +0900

    a

--- test.ts
+++ test.ts
@@ -8,17 +8,17 @@
 import {setup, LED,SERVO} from './src/sample'
 
 setup('COM4')
-// const myLED = LED(13);
+const myLED = LED(13);
 
 // myLED.blink();
 
 // myLED.blink()
-// myLED.on()
+myLED.on()
 // myLED.off()
-const servo = SERVO(10);
-servo.configureServo(1000,2000)
-servo.setAngle(90)
+// const servo = SERVO(10);
+// servo.configureServo(1000,2000)
+// servo.setAngle(90)
 
-setTimeout(()=>{
-    servo.setAngle(45);
-},2000)
+// setInterval(()=>{
+//     servo.setAngle(45);
+// },2000)
